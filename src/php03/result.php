602d26c08b718c92080224d004dfdb3e06452f26
memuni <pqinjia@gmail.com>
2024-09-23 00:25:21 +0900

    リモートリポジトリの変更

---
+++ src/php03/result.php
@@ -0,0 +1,26 @@
+</header>
+<main>
+  <div class="result__content">
+    <div class="result-cards">
+      <div class="result-card">
+        <div class="result-card__img-wrapper">
+          <img class="result-card__img" src="img/" alt="国旗">
+        </div>
+        <div class="result-card__body">
+          <p class="result-card__city"></p>
+          <p class="result-card__time"></p>
+        </div>
+      </div>
+      <div class="result-card">
+        <div class="result-card__img-wrapper">
+          <img class="result-card__img" src="img/" alt="国旗">
+        </div>
+        <div class="result-card__body">
+          <p class="result-card__city"></p>
+          <p class="result-card__time"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+</body>
